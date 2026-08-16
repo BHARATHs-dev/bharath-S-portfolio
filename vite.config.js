aabec04c2b2a1c07d23bcf645765fa0a59f52f54
bharath <bbharaths2202@gmail.com>
2026-08-16 14:19:37 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bharath-dev-portfolio/',
+  base: '/bharath-S-portfolio/',
   plugins: [react()],
   server: {
     proxy: {
